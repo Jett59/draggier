e86a70855840d9ee7951bec2a768f478be411a78
Jett Thompson <jett.thompson.59@gmail.com>
2020-04-20 16:21:18 +1000

    added string formating and \ns to consolify

--- src/main/java/com/mycodefu/draggier/Start.java
+++ src/main/java/com/mycodefu/draggier/Start.java
@@ -9,12 +9,21 @@ import java.util.regex.Pattern;
 public class Start {
 public static void main(String[] args) throws Exception {
 	List<String> code = Files.readAllLines(Paths.get(args[0]));
-	Pattern printPattern = Pattern.compile("consolifyln\\((.*?)\\)");
+	Pattern printlnPattern = Pattern.compile("consolifyln\\((.*?)\\)");
+	Pattern printfPattern = Pattern.compile("consolifyf\\((.*?), (.*?)\\)");
 	for(String line : code) {
-	Matcher printMatcher = printPattern.matcher(line);
-			if(printMatcher.find()) {
-				String content = printMatcher.group(1);
+	Matcher printlnMatcher = printlnPattern.matcher(line);
+			if(printlnMatcher.find()) {
+				String content = printlnMatcher.group(1);
+				content = content.replace("\\n", "\n");
 				System.out.println(content);
+			}
+			Matcher printfMatcher = printfPattern.matcher(line);
+			if(printfMatcher.find()) {
+				String text = printfMatcher.group(1);
+				String[] arguments = printfMatcher.group(2).split(", ");
+				text = text.replace("\\n", "\n");
+				System.out.printf(text, (Object[])arguments);
 			}
 }
 }
