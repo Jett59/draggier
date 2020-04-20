2b15826d142b272e1661bbcad17d9f529f903ced
Jett Thompson <jett.thompson.59@gmail.com>
2020-04-20 16:06:08 +1000

    first command, consolifyln

--- src/main/java/com/mycodefu/draggier/Start.java
+++ src/main/java/com/mycodefu/draggier/Start.java
@@ -1,5 +1,21 @@
 package com.mycodefu.draggier;
 
-public class Start {
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Start {
+public static void main(String[] args) throws Exception {
+	List<String> code = Files.readAllLines(Paths.get(args[0]));
+	Pattern printPattern = Pattern.compile("consolifyln\\((.*?)\\)");
+	for(String line : code) {
+	Matcher printMatcher = printPattern.matcher(line);
+			if(printMatcher.find()) {
+				String content = printMatcher.group(1);
+				System.out.println(content);
+			}
+}
+}
 }
