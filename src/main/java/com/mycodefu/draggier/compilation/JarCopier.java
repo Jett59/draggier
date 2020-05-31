75aa804f9d0ea53a17c4331683d4479e802483e3
Jett Thompson <jett.thompson.59@gmail.com>
2020-05-31 10:09:10 +1000

    added logging to jar copying

--- src/main/java/com/mycodefu/draggier/compilation/JarCopier.java
+++ src/main/java/com/mycodefu/draggier/compilation/JarCopier.java
@@ -9,7 +9,13 @@ public void copyJar(String jarPath) {
 	try {
 		File file = new File(jarPath);
 		String newPath = System.getenv("Draggier_home")+"\\"+file.getName();
+		System.out.printf("copying %s to %s\n", jarPath, newPath);
+		long now = System.nanoTime();
+		Files.deleteIfExists(Paths.get(newPath));
 		Files.copy(file.toPath(), Paths.get(newPath));
+		long finished = System.nanoTime();
+		finished -= now;
+		System.out.printf("copyed %s to %s, time: %ss\n", jarPath, newPath, Double.toString(finished/1000000000d));
 	}catch (Exception e) {
 		throw new CompilationException(e);
 	}
